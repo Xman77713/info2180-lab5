bd9b5df8cf5c5cee2d8257c6e6003f28391e094c
Pierre Mannix <pierre.mannix@mymona.uwi.edu>
2022-11-14 20:49:16 -0500

    Exercise 5

--- world.js
+++ world.js
@@ -1,3 +1,46 @@
 window.onload = function (){
+	let countryButton = document.getElementById("lookup-country");
+	let citiesButton = document.getElementById("lookup-cities");
 	
+	countryButton.addEventListener("click", function(e) {
+		e.preventDefault();
+		let country = document.getElementById("country").value;
+		let url = "http://localhost/info2180-lab5/world.php?country=" + country;
+		const httpRequest = new XMLHttpRequest();
+		httpRequest.onreadystatechange = function() {
+			if (httpRequest.readyState === XMLHttpRequest.DONE) {
+				if (httpRequest.status === 200) {
+					let response = httpRequest.responseText;
+					document.getElementById("result").innerHTML = response;
+				}
+				else {
+					alert('There was a problem with the request.');
+				}
+			}			
+		}
+		httpRequest.open('GET', url);
+		httpRequest.send();
+		
+	})
+	
+	citiesButton.addEventListener("click", function(e) {
+		e.preventDefault();
+		let country = document.getElementById("country").value;
+		let url = "http://localhost/info2180-lab5/world.php?country=" + country + "&lookup=cities";
+		const httpRequest = new XMLHttpRequest();
+		httpRequest.onreadystatechange = function() {
+			if (httpRequest.readyState === XMLHttpRequest.DONE) {
+				if (httpRequest.status === 200) {
+					let response = httpRequest.responseText;
+					document.getElementById("result").innerHTML = response;
+				}
+				else {
+					alert('There was a problem with the request.');
+				}
+			}			
+		}
+		httpRequest.open('GET', url);
+		httpRequest.send();
+		
+	})
 }
--- world.php
+++ world.php
@@ -4,15 +4,37 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $worldsearch = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING); //get query and sanitize
+$lookup = filter_input(INPUT_GET, "lookup", FILTER_SANITIZE_STRING);
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name = '{$worldsearch}'");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$worldsearch%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+if ($lookup == "cities") {
+	$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+	$stmt = $conn->query("SELECT cities.name,cities.district, cities.population FROM cities JOIN countries ON cities.country_code=countries.code WHERE countries.name LIKE '%$worldsearch%'");
+
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	?>
+	<table>
+		<tr><th>Name</th><th>District</th><th>Population</th></tr>
+	<?php foreach ($results as $row): ?>
+		<tr><td><?= $row['name']?></td><td><?= $row['district']; ?></td><td><?= $row['population']; ?></td></tr>
+	<?php endforeach; ?>
+	</table>
+	<?php
+}
+else {
+	?>
+	<table>
+		<tr><th>Name</th><th>Continent</th><th>Independence</th><th>Head of State</th></tr>
+	<?php foreach ($results as $row): ?>
+		<tr><td><?= $row['name']?></td><td><?= $row['continent']; ?></td><td><?= $row['independence_year']; ?></td><td><?= $row['head_of_state']; ?></td></tr>
+	<?php endforeach; ?>
+	</table>
+
+	<?php
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
